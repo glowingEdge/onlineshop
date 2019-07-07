150ede4b4cbb0337042a25da428575a50fba1dbf
glowingEdge <glowingEdge.com>
2019-07-07 13:59:53 +0900

    order앱생성

--- config/settings.py
+++ config/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.naver',
     'cart',
     'coupon',
+    'order',
 ]
 
 MIDDLEWARE = [
@@ -156,4 +157,4 @@ SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
 
-CART_ID = 'cart_in_session'
+CART_ID = None
