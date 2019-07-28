624cb5fbb805fd347db18a38a6b6aa6720ffe613
glowingEdge <glowingEdge.com>
2019-07-28 12:16:43 +0900

    Deployment version - config는 로컬

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.compute.amazonaws.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
